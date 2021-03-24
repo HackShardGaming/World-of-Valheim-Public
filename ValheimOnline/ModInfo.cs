296a7b33d265385d87bac640f317661f5e343a83
HackShardGaming <RobertChristopher@hackshardgmaing.net>
2021-03-24 14:46:58 -0400

    Updating Version # to 0.2.4 for release
    Changed version #

--- ValheimOnline/ModInfo.cs
+++ ValheimOnline/ModInfo.cs
@@ -11,7 +11,7 @@ namespace ValheimOnline
         public const string Group = "HackShardGaming";
         public const string Guid = "HackShardGaming.ValheimOnline";
         // Version follow Semantic Versioning Scheme (https://semver.org/)
-        public const string Version = "0.2.3";
+        public const string Version = "0.2.4";
         public const string buildDate = "2021-03-23";
 
         // Use GetBuildDate(Assembly.GetExecutingAssembly()); to get build date
