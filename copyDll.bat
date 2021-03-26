f08eaa2d432786364e506932f39be353ca7ae246
Reiuiji <Reiuiji@gmail.com>
2021-03-25 23:21:06 -0400

    CopyDll now supports both debug/release types on copy.

--- copyDll.bat
+++ copyDll.bat
@@ -4,17 +4,31 @@ REM  for testing. Chenge the path to what you need for testing.
 REM  
 REM Note: git will ignore changes
 REM
+ECHO "Running CopyDll Script"
+
+REM Input Args
+set APP_NAME=%1
+set CONFIG=%2
 
 REM Predetermine Paths
 set CUR_DIR=%~dp0
 set MOD_NAME=ValheimOnline
 set MOD_DIR=%CUR_DIR%\%MOD_NAME%
-set MOD_DLL=%MOD_DIR%\bin\Debug\%MOD_NAME%.dll
+set MOD_DLL=%MOD_DIR%\bin\%CONFIG%\%MOD_NAME%.dll
 set PLUGIN_DIR=BepInEx\plugins
+set OUTPUT_DIR=%MOD_DIR%\Output
 
 REM Development specific variables
 set SERVER_DIR=C:\Games\Valheim_servers\server1
 set PLAYER_DIR=C:\Program Files (x86)\Steam\steamapps\common\Valheim
+
+set "MOD_VER="
+    FOR /F "tokens=2 delims==" %%a in ('
+        wmic datafile where name^="%MOD_DLL:\=\\%" get Version /value 
+    ') do set "MOD_VER=%%a"
+
+echo "VERSION: %MOD_VER%"
+echo.
 
 REM Commence with the copying
 
@@ -32,4 +46,15 @@ if exist "%PLAYER_DIR%\%PLUGIN_DIR%" (
 	copy "%MOD_DLL%" "%PLAYER_DIR%\%PLUGIN_DIR%\%MOD_NAME%.dll"
 ) else (
 	echo "WARNING: Output Directory Does not exist."
+)
+
+if "%CONFIG%" == "Release" (
+    echo "Release Mode: Creating release packages (version: %MOD_VER%)"
+
+	if not exist "%OUTPUT_DIR%" (
+		echo "Creating output directory"
+		mkdir "%OUTPUT_DIR%"
+	)
+
+	"C:\Program Files\7-Zip\7z.exe" a %OUTPUT_DIR%\%MOD_NAME%-v%MOD_VER%.zip "%MOD_DLL%"
 )
