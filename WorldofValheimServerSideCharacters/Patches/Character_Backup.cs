12cb6ec9c42298bcae3872a1b13e740007636ff5
HackShardGaming <RobertChristopher@hackshardgaming.net>
2021-04-26 23:19:39 -0400

    Zones 0.5.16.  Adding Destructible type.  Also adding NoDamageToTrees.
    This is Zones 0.5.16 official patch
    Also internal testing on Server-Side Characters backups!

---
+++ WorldofValheimServerSideCharacters/Patches/Character_Backup.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Threading;
+using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using UnityEngine;
+
+
+namespace WorldofValheimServerSideCharacters
+{
+	public class Character_Backup
+    {
+		// Lets create the BackupCharacter Thread!
+		public static Thread BackupCharacter;
+		// Here is the CopyFolder
+		public static void CopyCharacters(string SavePath, string BackupPath)
+		{
+			if (!Directory.Exists(BackupPath))
+			Directory.CreateDirectory(BackupPath);
+			string[] folders = Directory.GetDirectories(SavePath);
+			foreach (string folder in folders)
+			{
+				CopyCharacters(folder, Path.Combine(BackupPath, Path.GetFileName(folder)));
+			}
+		}
+		public static void BackupScanner()
+		{
+			Debug.Log("Starting Initial backup!");
+			Backup_Characters(DateTime.Now.ToString("yyyy-MM-dd_HH-mm"));
+			while (true)
+			{
+				// Sleep the thread for (X) minutes.
+				Thread.Sleep(WorldofValheimServerSideCharacters.BackupInterval.Value * 60 * 1000);
+				Backup_Characters(DateTime.Now.ToString("yyyy-MM-dd_HH"));
+			}
+		}
+		public static void Backup_Characters(string CurrentTime)
+		{
+			var SavePath = WorldofValheimServerSideCharacters.CharacterSavePath.Value;
+			var BackupPath = Path.Combine(SavePath + ".bak", CurrentTime);
+			Debug.Log("Attempting to backup all WoV-SSC Character files!");
+			IOrderedEnumerable<FileSystemInfo> BackupDirectory = new DirectoryInfo(SavePath + ".bak").GetFileSystemInfos().OrderByDescending(fi => fi.CreationTime);
+			if (BackupDirectory.Count() > WorldofValheimServerSideCharacters.MaxBackups.Value - 1)
+			{
+				Directory.Delete(BackupDirectory.Last().FullName, true);
+			}
+			CopyCharacters(SavePath, BackupPath);
+			Debug.Log("WoV-SSC Character files have been backed up!");
+		}
+    }
+}
